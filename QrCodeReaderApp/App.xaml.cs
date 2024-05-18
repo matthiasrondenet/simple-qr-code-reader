f10d50295002dd1ec38ed8d626ccbc64ca5fafe9
matthiasrondenet <matthiasrondenet@gmail.com>
2024-05-18 17:06:07 +0200

    maui template

---
+++ QrCodeReaderApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QrCodeReaderApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ QrCodeReaderApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QrCodeReaderApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
